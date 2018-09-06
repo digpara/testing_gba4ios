4c61b16e8d2a9e5d71e396ffbf9e66562c6f2447
nebulabox <nebualbox@gmail.com>
2018-09-06 21:35:18 +0800

    fix build for iOS 11

--- emu-ex-plus-alpha/EmuFramework/src/FileUtils.cc
+++ emu-ex-plus-alpha/EmuFramework/src/FileUtils.cc
@@ -11,19 +11,19 @@ void chdirFromFilePath(const char *path)
 
 void fixFilePermissions(const char *path)
 {
-	if(FsSys::hasWriteAccess(path) == 0)
-	{
-		logMsg("%s lacks write permission, setting user as owner", path);
-	}
-	else
-		return;
-
-	FsSys::cPath execPath;
-	string_printf(execPath, "%s/fixMobilePermission '%s'", Base::appPath, path);
-	//logMsg("executing %s", execPath);
-	int err = system(execPath);
-	if(err)
-	{
-		logWarn("error from fixMobilePermission helper: %d", err);
-	}
+//    if(FsSys::hasWriteAccess(path) == 0)
+//    {
+//        logMsg("%s lacks write permission, setting user as owner", path);
+//    }
+//    else
+//        return;
+//
+//    FsSys::cPath execPath;
+//    string_printf(execPath, "%s/fixMobilePermission '%s'", Base::appPath, path);
+//    //logMsg("executing %s", execPath);
+//    int err = system(execPath);
+//    if(err)
+//    {
+//        logWarn("error from fixMobilePermission helper: %d", err);
+//    }
 }
